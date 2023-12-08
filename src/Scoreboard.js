46e96c115b7c16ddfd6e33dfbc865241a3fb1607
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-12-08 14:00:02 -0600

    Add header and footer, update styles

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,8 +1,8 @@
 function Scoreboard() {
     return (
-        <div>
-            <h1>Scoreboard</h1>
-            <button className="btn-round">hey</button>
+        <div className="pad-ver-1">
+            <h1 className="txt-center">Scoreboard</h1>
+            <button className="btn-round bdr-3 bdr-solid bdr-black">button</button>
         </div>
     );
 }
