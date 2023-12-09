7854493287901b6fa055f689983b27caa8cebf03
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-12-09 10:43:55 -0600

    Finish component layouts

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,7 +1,53 @@
 function PlayerCard() {
     return (
         <div className="card">
-            <i className="fa-solid fa-xmark close"></i>
+                <i className="fa-solid fa-xmark close"></i>
+
+            <div className="name-row">
+                <h3 className="name">Player</h3>
+
+                <div className="bid-history flex-wrap gap-half">
+                    {/* put the stuff here */}
+                    <div className="past-bid right"></div>
+                    <div className="past-bid wrong"></div>
+                    <div className="past-bid right"></div>
+                    <div className="past-bid right"></div>
+                    <div className="past-bid right"></div>
+                    <div className="past-bid wrong"></div>
+                    <div className="past-bid right"></div>
+                    <div className="past-bid wrong"></div>
+                </div>
+            </div>
+
+            <div className="score-row">
+                <div className="bid">
+                    <button className="bid-btn bdr-round-1 bdr-3 bdr-solid bdr-white bg-black txt-white">
+                        <i className="fa-solid fa-plus"></i>
+                    </button>
+
+                    <p className="bid-num">
+                        88
+                    </p>
+
+                    <button className="bid-btn bdr-round-1 bdr-3 bdr-solid bdr-white bg-black txt-white">
+                        <i className="fa-solid fa-minus"></i>
+                    </button>
+                </div>
+
+                <p className="score number">
+                    888
+                </p>
+
+                <div className="yes-no">
+                    <button className="yes-no-btn plus bdr-4 bdr-solid bdr-green bg-black txt-green">
+                        <i className="fa-solid fa-check"></i>
+                    </button>
+
+                    <button className="yes-no-btn minus bdr-4 bdr-solid bdr-red bg-black txt-red">
+                        <i className="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,7 +3,7 @@ import PlayerCard from "./PlayerCard";
 
 function Scoreboard() {
     return (
-        <div className="flex-center flex-wrap">
+        <div className="flex-center flex-wrap gap-2">
             {/* put player cards here */}
             <PlayerCard />
             <AddPlayerBtn />
