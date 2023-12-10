b352eed34ca457440a54501cbd8efe06393775ad
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-12-10 15:11:15 -0600

    Add comments

--- src/components/AddPlayerBtn.js
+++ src/components/AddPlayerBtn.js
@@ -1,5 +1,6 @@
 function AddPlayerBtn() {
     return (
+        // function
         <button type="button" id="add-player-btn" className="bdr-5 bdr-solid bdr-black">
             <i class="fa-solid fa-user"></i>
             <i class="fa-solid fa-plus"></i>
--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,13 +1,14 @@
 function PlayerCard() {
     return (
         <div className="card">
-                <i className="fa-solid fa-xmark close"></i>
+            <i className="fa-solid fa-xmark close"></i>
 
             <div className="name-row">
+                {/* prop */}
                 <h3 className="name">Player</h3>
 
                 <div className="bid-history flex-wrap gap-half">
-                    {/* put the stuff here */}
+                    {/* list? */}
                     <div className="past-bid right"></div>
                     <div className="past-bid wrong"></div>
                     <div className="past-bid right"></div>
@@ -21,28 +22,34 @@ function PlayerCard() {
 
             <div className="score-row">
                 <div className="bid">
+                    {/* function */}
                     <button className="bid-btn bdr-round-1 bdr-3 bdr-solid bdr-white bg-black txt-white">
                         <i className="fa-solid fa-plus"></i>
                     </button>
 
+                    {/* prop */}
                     <p className="bid-num">
                         88
                     </p>
 
+                    {/* function */}
                     <button className="bid-btn bdr-round-1 bdr-3 bdr-solid bdr-white bg-black txt-white">
                         <i className="fa-solid fa-minus"></i>
                     </button>
                 </div>
 
+                {/* prop */}
                 <p className="score number">
                     888
                 </p>
 
                 <div className="yes-no">
+                    {/* function */}
                     <button className="yes-no-btn plus bdr-4 bdr-solid bdr-green bg-black txt-green">
                         <i className="fa-solid fa-check"></i>
                     </button>
 
+                    {/* function */}
                     <button className="yes-no-btn minus bdr-4 bdr-solid bdr-red bg-black txt-red">
                         <i className="fa-solid fa-xmark"></i>
                     </button>
