35c3ec3c4906819fe7d5b6e6ae60db747922321f
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2024-09-20 10:38:20 -0500

    Restructure to use App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Scoreboard from './components/Scoreboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <Scoreboard />
+        <App />
     </React.StrictMode>
 );
 
