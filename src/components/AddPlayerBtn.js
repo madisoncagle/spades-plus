a578c840f27bb3aa6e491386633ecedc60ddd0dc
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2024-10-30 17:38:04 -0500

    Begin color and font change

--- src/components/AddPlayerBtn.js
+++ src/components/AddPlayerBtn.js
@@ -1,6 +1,6 @@
 function AddPlayerBtn({ onAdd }) {
     return (
-        <button type="button" id="add-player-btn" className="bdr-5 bdr-solid bdr-black" onClick={onAdd}>
+        <button type="button" id="add-player-btn" onClick={onAdd}>
             <i className="fa-solid fa-user"></i>
             <i className="fa-solid fa-plus"></i>
         </button>
