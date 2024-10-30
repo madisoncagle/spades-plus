ad1bae0b1d9dd0a05bd683edb0c2b9deba16e562
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2024-10-30 14:24:39 -0500

    Restore old new player button

--- src/components/AddPlayerBtn.js
+++ src/components/AddPlayerBtn.js
@@ -1,19 +1,8 @@
 function AddPlayerBtn({ onAdd }) {
     return (
-        // <button type="button" id="add-player-btn" className="bdr-5 bdr-solid bdr-black" onClick={onAdd}>
-        //     <i className="fa-solid fa-user"></i>
-        //     <i className="fa-solid fa-plus"></i>
-        // </button>
-
-        <button type="button" id="add-player-btn" onClick={onAdd}>
-            <span className="name-line">New player</span>
-
-            <span className="add-player-plus">+</span>
-
-            <div className="tiny-rectangles">
-                <div className="tiny-rectangle red"></div>
-                <div className="tiny-rectangle blue"></div>
-            </div>
+        <button type="button" id="add-player-btn" className="bdr-5 bdr-solid bdr-black" onClick={onAdd}>
+            <i className="fa-solid fa-user"></i>
+            <i className="fa-solid fa-plus"></i>
         </button>
     );
 }
